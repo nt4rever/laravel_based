4c3d6a395036159c96b8dfcd111b8e77945c84d5
nt4rever <levantanald@gmail.com>
2023-07-12 22:50:02 +0700

    dockerize php laravel nginx postgres

---
+++ src/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Enums\Status;
+use App\Enums\UserRole;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\User::factory(10)->create();
+
+        \App\Models\User::factory()->create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => '123456',
+            'type' => UserRole::ADMIN,
+            'status' => Status::ACTIVE
+        ]);
+        // $this->call([
+        //     AddUser::class
+        // ]);
+    }
+}
